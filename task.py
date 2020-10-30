ad7c073b435e1d4f02400781d614bd3334fc1439
Mateusz Lata <mateuszlata2@gmail.com>
2020-10-30 12:45:33 +0100

    initial commit

--- task.py
+++ task.py
@@ -1,30 +1,35 @@
-# Class diary
-#
-# Create program for handling lesson scores.
-# Use python to handle student (highscool) class scores, and attendance.
-# Make it possible to:
-# - Get students total average score (average across classes)
-# - get students average score in class
-# - hold students name and surname
-# - Count total attendance of student
-#
-# Please, use your imagination and create more functionalities.
-# Your project should be able to handle entire school(s?).
-# If you have enough courage and time, try storing (reading/writing)
-# data in text files (YAML, JSON).
-# If you have even more courage, try implementing user interface (might be text-like).
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#
-#Delete these comments before commit!
-#Good luck.
+class Student:
+    def __init__(self, name, surname, grade):
+        self.name = name
+        self.surname = name
+        self.grade = grade
+        self.marks = []
+        self.avg = 0
+        self.attendance = 0
+        self.attendancePer = 0
+
+    def addGrade(self, mark):
+        self.marks.append(mark)
+
+    def calcAverage(self):
+        sum = 0
+        for i in range(len(self.marks)):
+            sum = sum + self.marks[i]
+
+        self.avg = sum / len(self.marks)
+
+    def checkAttendance(self, bool1):
+        if bool1 == true:
+            self.attendance += 1
+
+    def checkTotalAttendance(self, NoOfClasses):
+        self.attendancePer = self.attendance / NoOfClasses
+
+
+st1 = Student('Mateusz', 'Kata', 1)
+st1.addGrade(5)
+st1.addGrade(6)
+print(st1.name)
+print(st1.marks)
+st1.calcAverage()
+print(st1.avg)
